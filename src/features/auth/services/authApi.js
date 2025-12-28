e1d8f6f62f027c5341bdf362773196950a86436f
quocanh <quocanh.dev404@gmail.com>
2025-12-28 22:04:22 +0700

    feat: forgot and reset password

--- src/features/auth/services/authApi.js
+++ src/features/auth/services/authApi.js
@@ -3,4 +3,7 @@ import { BE_BASE_URL } from "../../../common/constants/env";
 export const authEndPoints = {
     LOGIN_API: BE_BASE_URL + "/account/login",
     SIGN_UP_API: BE_BASE_URL + "/account/register",
+    FORGOT_PASSWORD_API: BE_BASE_URL + "/auth/forgot-password",
+    VALIDATE_RESET_TOKEN_API: BE_BASE_URL + "/auth/validate-reset-token",
+    RESET_PASSWORD_API: BE_BASE_URL + "/auth/reset-password",
 };
