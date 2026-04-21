99a178d1c95699e813c14b86b1b2016f8bdabba9
hoanghung256 <hoanghung250604@gmail.com>
2026-04-21 22:47:16 +0700

    fix: wrap EmptyLayout in CollectQuestionTrayProvider for improved context management

--- src/app/layouts/EmptyLayout.jsx
+++ src/app/layouts/EmptyLayout.jsx
@@ -2,16 +2,17 @@ import { Outlet } from "react-router-dom";
 import SuspendedGate from "../../common/components/SuspendedGate";
 import CandidateAssessmentGate from "../../common/components/CandidateAssessmentGate";
 import usePageTracking from "../../hooks/usePageTracking";
+import { CollectQuestionTrayProvider } from "../../common/context/CollectQuestionTrayContext";
 
 function EmptyLayout() {
     usePageTracking();
     return (
-        <>
+        <CollectQuestionTrayProvider>
             <CandidateAssessmentGate />
             <SuspendedGate>
                 <Outlet />
             </SuspendedGate>
-        </>
+        </CollectQuestionTrayProvider>
     );
 }
 
